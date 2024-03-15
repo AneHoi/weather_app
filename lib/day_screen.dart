28580466f966cf963ff4623ff627eecd3f7957fe
Anehoimark <anehoimark@gmail.com>
2024-03-15 14:24:31 +0100

    changed with web

--- lib/day_screen.dart
+++ lib/day_screen.dart
@@ -1,92 +1,120 @@
-import 'dart:async';
-
+import 'package:community_charts_flutter/community_charts_flutter.dart'
+as charts;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:weather/chart_screen.dart';
 
 import 'data_source.dart';
-import 'models.dart';
-import 'weekly_forecast_list.dart';
-import 'weather_sliver_app_bar.dart';
+import 'models/time_series.dart';
 
-class DayScreen extends StatefulWidget {
+class DayScreen extends StatelessWidget {
   const DayScreen({super.key});
-
-  @override
-  State<DayScreen> createState() => _DayScreenState();
-}
-
-class _DayScreenState extends State<DayScreen> {
-  final controller = StreamController<WeeklyForecastDto>();
-
-  @override
-  void initState() {
-    super.initState();
-    loadForecast();
-  }
-
-  Future<void> loadForecast() async {
-    final future = context.read<DataSource>().getWeeklyForecast();
-    controller.addStream(future.asStream());
-    await future;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: controller.stream,
-        builder: (context, snapshot) =>
-            CustomScrollView(
-              slivers: <Widget>[
-                WeatherSliverAppBar(onRefresh: loadForecast),
-                if (snapshot.hasData)
-                  WeeklyForecastList(weeklyForecast: snapshot.data!)
-                else
-                  if (snapshot.hasError)
-                    _buildError(snapshot, context)
-                  else
-                    _buildSpinner()
+      body: SafeArea(
+        child: FutureBuilder<WeatherChartData>(
+          future: context.read<DataSource>().getChartData(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const CircularProgressIndicator();
+            final variables = snapshot.data!.daily!;
+            charts.DateTimeAxisSpec(
+              tickFormatterSpec: charts.BasicDateTimeTickFormatterSpec(
+                    (datetime) => DateFormat("E").format(datetime),
+              ),
+            );
+
+            final axisColor = charts.MaterialPalette.yellow.shadeDefault;
+            charts.Color minTemp = charts.ColorUtil.fromDartColor(
+                Color.fromRGBO(32, 150, 229, 1.0));
+            charts.Color maxTemp = charts.ColorUtil.fromDartColor(
+                Color.fromRGBO(229, 68, 32, 1.0));
+
+            return charts.TimeSeriesChart(
+              [
+                charts.Series<TimeSeriesDatum, DateTime>(
+                  id: 'Max',
+                  domainFn: (datum, _) => datum.domain,
+                  measureFn: (datum, _) => datum.measure,
+                  colorFn: (datum, _) => maxTemp,
+                  data: variables
+                      .singleWhere(
+                          (variable) => variable.name == "temperature_2m_max")
+                      .values,
+                ),
+                charts.Series<TimeSeriesDatum, DateTime>(
+                  id: 'Min',
+                  domainFn: (datum, _) => datum.domain,
+                  measureFn: (datum, _) => datum.measure,
+                  colorFn: (datum, _) => minTemp,
+                  data: variables
+                      .singleWhere(
+                          (variable) => variable.name == "temperature_2m_min")
+                      .values,
+                ),
               ],
-            ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder:
-                      (context) => ChartScreen()
-              )
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
 
-  Widget _buildSpinner() {
-    return const SliverFillRemaining(
-      hasScrollBody: false,
-      child: Center(
-        child: CircularProgressIndicator.adaptive(),
-      ),
-    );
-  }
+              /// Assign a custom style for the domain axis. x-axis
+              domainAxis: charts.DateTimeAxisSpec(
+                tickFormatterSpec: charts.BasicDateTimeTickFormatterSpec(
+                        (datetime) {
+                      // Define Danish day names
+                      List<String> danishDayNames = ['man', 'tir', 'ons', 'tor', 'fre', 'lør', 'søn'];
 
-  Widget _buildError(AsyncSnapshot<WeeklyForecastDto> snapshot,
-      BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Text(
-          snapshot.error.toString(),
-          style: TextStyle(color: Theme
-              .of(context)
-              .colorScheme
-              .error),
+                      // Get the day of the week index (0 for Monday, 1 for Tuesday, etc.)
+                      int dayOfWeekIndex = datetime.weekday - 1;
+
+                      // Get the corresponding Danish day name
+                      String danishDayName = danishDayNames[dayOfWeekIndex];
+
+                      return danishDayName;
+                    }
+                ),
+                renderSpec: charts.SmallTickRendererSpec(
+                  // Tick and Label styling here.
+                  labelStyle: charts.TextStyleSpec(color: axisColor),
+                  // Change the line colors to match text color.
+                  lineStyle: charts.LineStyleSpec(color: axisColor),
+                ),
+              ),
+
+
+              /// Assign a custom style for the measure axis. y-axis
+              primaryMeasureAxis: charts.NumericAxisSpec(
+                tickProviderSpec: charts.BasicNumericTickProviderSpec(desiredTickCount: 6), // Adjust the desiredTickCount as needed
+
+                renderSpec: charts.GridlineRendererSpec(
+                  // Tick and Label styling here.
+                  labelStyle: charts.TextStyleSpec(color: axisColor),
+                  // Change the line colors to match text color.
+                  lineStyle: charts.LineStyleSpec(color: axisColor),
+                ),
+              ),
+
+              animate: true,
+              defaultRenderer: charts.LineRendererConfig(includePoints: true),
+              dateTimeFactory: const charts.LocalDateTimeFactory(),
+              behaviors: [
+                charts.SeriesLegend(),
+                charts.ChartTitle(
+                  "Temperatur",
+                  titleStyleSpec: charts.TextStyleSpec(
+                    color: charts.MaterialPalette.pink.shadeDefault,
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),*/
     );
   }
 }
